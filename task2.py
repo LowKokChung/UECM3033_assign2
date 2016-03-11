2ef1fb175296989bda2f05f641692d907ed93798
LowKokChung <kokchung2010@1utar.my>
2016-03-11 11:09:32 +0800

    Updated task 2.py

--- task2.py
+++ task2.py
@@ -3,23 +3,8 @@ import matplotlib.image as mpimg
 import numpy as np
 import scipy.linalg as lg
 
-img=mpimg.imread ('Handsome.jpg')
-[r,g,b] = [img[:,:,i] for i in range(3)]
 
-
-fig = plt.figure(1)
-ax1 = fig.add_subplot(2,2,1)
-ax2 = fig.add_subplot(2,2,2)
-ax3 = fig.add_subplot(2,2,3)
-ax4 = fig.add_subplot(2,2,4)
-ax1.imshow(img)
-ax2.imshow(r, cmap = 'Reds')
-ax3.imshow(g, cmap = 'Greens')
-ax4.imshow(b, cmap = 'Blues')
-plt.show()
-
-#other picture
-img2=mpimg.imread('Lenna.jpg')
+img2=mpimg.imread('Handsome.jpg')
 [r2,g2,b2] = [img2[:,:,i] for i in range(3)]
 
 red = {'Index' : 0,'Mat': r2, 'Color' : 'Reds'}
@@ -39,11 +24,7 @@ for color in rgb:
     
     for i in range(s.size):
         S[i][i] = s[i]
-        
-    #check: Get back A
-    #check = np.dot(np.dot(U,S),V)
-    #plt.imshow(check, cmap ='Reds')
-    #plt.show()
+     
     color['U'] = U
     color['eigenval'] = s
     color['S'] = S
@@ -61,7 +42,7 @@ for dimension in (30,200):
         img_new[:,:,color['Index']]= np.dot(np.dot(color['U'],S_new),color['V'])
     plt.imshow(img_new)
     if (dimension == 30 ):
-        plt.imsave('Lenna_lower.jpg',img_new)
+        plt.imsave('Handsome_lower_resolution.jpg',img_new)
     if (dimension == 200):
-        plt.imsave('Lenna_better.jpg',img_new)
+        plt.imsave('Handsome_better_resolution.jpg',img_new)
     plt.show()
