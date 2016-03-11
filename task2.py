7f438fe70c97a525bad66837f400b38e2309bfd0
LowKokChung <kokchung2010@1utar.my>
2016-03-11 11:32:43 +0800

    Updated task 2.py

--- task2.py
+++ task2.py
@@ -12,37 +12,45 @@ green = {'Index' : 1, 'Mat': g2, 'Color' : 'Greens'}
 blue = {'Index' : 2, 'Mat': b2, 'Color' : 'Blues'}
 rgb = [red,green,blue]
 
-for color in rgb:
-    plt.imshow(color['Mat'], cmap = color['Color'])
+for layer in rgb:
+    plt.imshow(layer['Mat'], cmap = layer['Color'])
     plt.show()
 
-
 # SVD decomposition of img
-    U,s,V = lg.svd(color['Mat'])
+    U,s,V = lg.svd(layer['Mat'])
     
-    S = np.zeros(color['Mat'].shape,s.dtype)
+    S = np.zeros(layer['Mat'].shape,s.dtype)
     
     for i in range(s.size):
         S[i][i] = s[i]
-     
-    color['U'] = U
-    color['eigenval'] = s
-    color['S'] = S
-    color['V'] = V
+       
+    layer['U'] = U
+    layer['eigenval'] = s
+    layer['S'] = S
+    layer['V'] = V
     
 img_new = np.zeros_like(img2)
 
-for dimension in (30,200):
-    for color in rgb:
-        S_new = np.zeros(color['Mat'].shape,s.dtype)
+dimension = 30
+for layer in rgb:
+    S_30 = np.zeros(layer['Mat'].shape,s.dtype)
+    
+    for i in range(dimension):
+        S_30[i][i] = layer['eigenval'][i]
         
-        for i in range(dimension):
-            S_new[i][i] = color['eigenval'][i]
-            
-        img_new[:,:,color['Index']]= np.dot(np.dot(color['U'],S_new),color['V'])
-    plt.imshow(img_new)
-    if (dimension == 30 ):
-        plt.imsave('Handsome_lower_resolution.jpg',img_new)
-    if (dimension == 200):
-        plt.imsave('Handsome_better_resolution.jpg',img_new)
-    plt.show()
+    img_new[:,:,layer['Index']]= np.dot(np.dot(layer['U'],S_30),layer['V'])
+plt.imshow(img_new)
+plt.imsave('Handsome_lower_resolution.jpg',img_new)
+plt.show()
+
+dimension = 200
+for layer in rgb:
+    S_200 = np.zeros(layer['Mat'].shape,s.dtype)
+    
+    for i in range(dimension):
+        S_200[i][i] = layer['eigenval'][i]
+        
+    img_new[:,:,layer['Index']]= np.dot(np.dot(layer['U'],S_200),layer['V'])
+plt.imshow(img_new)
+plt.imsave('Handsome_better_resolution.jpg',img_new)
+plt.show()
